f84422439bcbd2c7de780941cb5c09019b595466
Rayza Arasj Mahardhika <rayza.arasj@gmail.com>
2024-08-30 14:52:26 +0900

    add comment

--- src/Scripts/Scenes/GameScene.ts
+++ src/Scripts/Scenes/GameScene.ts
@@ -2,6 +2,7 @@ import "phaser";
 import { Bubble } from "../GameObjects/Bubble";
 import { GameOverPanel } from "../GameObjects/GameOverPanel";
 
+// TEST
 export class GameScene extends Phaser.Scene {
   colors: Object;
   columns: integer;
